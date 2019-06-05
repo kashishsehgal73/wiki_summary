aceec61ba0ee31dbdbb3f52efe866a0bcf2f9a67
kashishsehgal73 <kashishsehgal73@gmail.com>
2019-06-05 22:40:14 +0530

    search bar

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask,request,render_template
 import wikipedia
 app = Flask(__name__)
-
 @app.route("/", methods =['POST', 'GET'])
 def index():
 	if request.method == 'GET':
@@ -10,33 +9,23 @@ def index():
 		return handle_query()
 
 def handle_query():
-	summary = '<h1>The summary is: </h1>'
 	topic = request.form.get("topic")
+	summary = '<h1>{}</h1>'.format(topic)
 	if topic:
 		try:
 			summary = summary + wikipedia.summary(str(topic))
 		except wikipedia.exceptions.DisambiguationError as e:
-			summary = "<h2>There is an ambiguiy!!</h2> <br> <h3>Please enter from the following options:</h3> <br>" + "<br> ".join(e.options[:20])
+			summary = "<h2>There is an ambiguty!!</h2> <h3>Please enter from the following options:</h3> <br>" + "<br> ".join(e.options[:20])
 		except wikipedia.exceptions.PageError:
 			summary = "<h2>No Page found!!</h2> <br> <h3>Please try another query</h3> <br>"
 	else:
-		summary = "<h1>Empty query!!!</h1>"
+		summary = "<h3 style='color:red'>Empty query!!!</h3>"
 	return render_template("summary.html",summary = summary)
 
-
-@app.route('/login', methods =['POST', 'GET'])
-def login():
-	if request.method == 'GET':
-		return render_template("login.html")
-	else:
-		user, password = request.form.get("user"),request.form.get("pass")
-		print(user,password)
-		return "success"
 @app.errorhandler(404)
 def not_found_error(error):
     return (render_template('error.html',code = "404"), 404)
 
-#Error handler for 500
 @app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
